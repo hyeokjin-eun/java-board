0065157e2ca6c20081ed81e86c4e6a2e0a2430db
hyeokjin-eun <gurwls2399@gmail.com>
2020-02-24 11:23:16 +0900

    first commit

---
+++ src/main/java/com/board/java/JavaBaordApplication.java
@@ -0,0 +1,13 @@
+package com.board.java;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaBaordApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JavaBaordApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/board/java/JavaBaordApplicationTests.java
@@ -0,0 +1,13 @@
+package com.board.java;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaBaordApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
