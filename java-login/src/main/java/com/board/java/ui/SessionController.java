14fdbbfe395430383eb01acb7fe88618e96c60d5
hyeokjin-eun <gurwls2399@gmail.com>
2020-03-17 23:38:03 +0900

    로그인 기능 개발
    Application java 추가

--- java-login/src/main/java/com/board/java/ui/SessionController.java
+++ java-login/src/main/java/com/board/java/ui/SessionController.java
@@ -1,10 +1,16 @@
 package com.board.java.ui;
 
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("session")
 public class SessionController {
-
+    @PostMapping("")
+    public ResponseEntity<?> create() {
+        return ResponseEntity.ok()
+                .body("");
+    }
 }
