964b420b778108c1cec440a4680775a6649eba6d
hyeokjin-eun <gurwls2399@gmail.com>
2020-03-29 23:53:08 +0900

    로그인 기능 개발
    Spring 시큐리티 적용중
    authenticate Service 개발중

--- java-login/src/main/java/com/board/java/application/UserService.java
+++ java-login/src/main/java/com/board/java/application/UserService.java
@@ -1,13 +1,26 @@
 package com.board.java.application;
 
+import com.board.java.domain.entity.User;
 import com.board.java.domain.request.SessionApiRequest;
 import com.board.java.domain.response.SessionApiResponse;
+import com.board.java.infra.UserRepository;
 import org.springframework.stereotype.Service;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 @Service
 public class UserService {
 
+    private UserRepository userRepository;
+
+    private PasswordEncoder passwordEncoder;
+
     public SessionApiResponse authenticate(SessionApiRequest sessionApiRequest) {
+        User user = userRepository.findByEmail(sessionApiRequest.getEmail())
+                .orElseThrow(RuntimeException::new);
+
+        if (passwordEncoder.matches(sessionApiRequest.getPassword(), user.getPassword())) {
+            throw new RuntimeException();
+        }
         return null;
     }
 }
