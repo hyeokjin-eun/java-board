f8be1600e9750651518b9ac77e0336cccf1d3a4a
hyeokjin-eun <gurwls2399@gmail.com>
2020-03-30 23:58:08 +0900

    로그인 기능 개발
    Security Configuration Setting중

--- java-login/src/main/java/com/board/java/application/UserService.java
+++ java-login/src/main/java/com/board/java/application/UserService.java
@@ -4,15 +4,22 @@ import com.board.java.domain.entity.User;
 import com.board.java.domain.request.SessionApiRequest;
 import com.board.java.domain.response.SessionApiResponse;
 import com.board.java.infra.UserRepository;
-import org.springframework.stereotype.Service;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
 
 @Service
 public class UserService {
 
-    private UserRepository userRepository;
+    private final UserRepository userRepository;
 
-    private PasswordEncoder passwordEncoder;
+    private final PasswordEncoder passwordEncoder;
+
+    @Autowired
+    public UserService(UserRepository userRepository, PasswordEncoder passwordEncoder) {
+        this.userRepository = userRepository;
+        this.passwordEncoder = passwordEncoder;
+    }
 
     public SessionApiResponse authenticate(SessionApiRequest sessionApiRequest) {
         User user = userRepository.findByEmail(sessionApiRequest.getEmail())
